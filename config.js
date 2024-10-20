22817519376d81429683960b11a8ce556b2b0081
NotCodingWell <139487315+NotCodingWell@users.noreply.github.com>
2024-10-20 20:54:32 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,20 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
+    titleWeb: "Hello con Chuột",
     introTitle: 'Tên người yêu bạn',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
+    title: 'Ngày 20/10 vui vẻ nha đồ ngốc',
+    desc: '',
+    btnYes: 'Cảm ơn nhaaaa',
+    btnNo: 'Không cần?',
+    question: 'Thấy tao làm công phu không =)))',
+    btnReply: 'Gửi',
+    reply: 'Quá công phu luôn yêu heo <33',
+    mess: 'Heh, trình tao mà =)))',
+    messDesc: 'Thấy hay thì nhớ qua mess cảm ơn nha',
+    btnAccept: 'Okeeee lun, yêu heo <3',
+    messLink: 'https://www.facebook.com/messages/e2ee/t/7758318674205362'
 }
